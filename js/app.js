14c7fc71b288bdd1ffabc5995c5ff6710fd22a94
lfernandortiz <lfernandortiz@gmail.com>
2016-11-09 18:44:10 -0500

    nuevo proyecto

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation()
+$(window).scroll(function() {
+      if ($(this).scrollTop() > 2){
+        $('#stickyMenu').addClass("barMenuSticky");
+      }
+      else{
+        $('#stickyMenu').removeClass("barMenu");
+      }
+      });
