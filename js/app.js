34457127fa3f96024fb85dc7ca66da6dfb0a887c
lfernandortiz <lfernandortiz@gmail.com>
2016-11-09 18:47:37 -0500

    añadiendo fuentes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,1 @@
 $(document).foundation()
-$(window).scroll(function() {
-      if ($(this).scrollTop() > 2){
-        $('#stickyMenu').addClass("barMenuSticky");
-      }
-      else{
-        $('#stickyMenu').removeClass("barMenu");
-      }
-      });
