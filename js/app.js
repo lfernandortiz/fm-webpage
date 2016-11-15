0424fdb543976819adf81b2683d251f6af922426
lfernandortiz <lfernandortiz@gmail.com>
2016-11-15 15:19:56 -0500

    Menu ready

--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@ $(document).foundation();
 $('.off-canvas a').on('click', function() {
   $('.off-canvas').foundation('close');
 });
+
+
+  $('.menu li a').click(function(){
+    $('li a').removeClass("activation");
+    $(this).addClass("activation");
+});
