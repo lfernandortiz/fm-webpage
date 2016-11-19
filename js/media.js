1e32f6d409462229adf61c1cb663f33624de6e0a
lfernandortiz <lfernandortiz@gmail.com>
2016-11-19 08:32:50 -0500

    cambios para el formularios

--- js/media.js
+++ js/media.js
@@ -1,5 +1,43 @@
+var asyncRequest;
 function iniciar() {
-	var sonido = document.getElementById('jigle');
+	var sonido = document.getElementById("jigle");
 	sonido.play();
+
+	var botonForm = document.getElementById("enviarForm");
+	var botonFormSmall = document.getElementById("enviarFormSmall");
+	botonForm.addEventListener("click",  enviarFormulario, false);
+	botonFormSmall.addEventListener("click",  enviarFormulario, false);
 }
+
+function enviarFormulario(){	
+	var url = "modules/processform.php?";
+	var nombre = document.getElementById("nombreCliente").value;
+	var email = document.getElementById("emailCliente").value;
+	var telefono = document.getElementById("telefonoCliente").value;
+	var comentario = document.getElementById("comentarioCliente").value;
+	url += "name="+nombre+"&email="+email+"&telefono="+telefono+"&comentario="+comentario;	
+	console.log(url)
+	try{
+		asyncRequest = new XMLHttpRequest();
+		asyncRequest.addEventListener(
+			"readystatechange", stateChange, false);
+		asyncRequest.open("GET", url, true); 
+		asyncRequest.send(null);
+	}catch(excepcion){}
+}
+
+function stateChange() {
+	if (asyncRequest.readyState == 4 && asyncRequest.status == 200) {		
+		var response = asyncRequest.responseText;
+		console.log(">"+response);
+		if(response === "true"){			
+			document.getElementById("nombreCliente").value = "";
+			document.getElementById("emailCliente").value = "";
+			document.getElementById("telefonoCliente").value = "";
+			document.getElementById("comentarioCliente").value = "";
+			document.getElementById("calloutForm").style.display = 'block';
+		}			 
+	} 
+}
+
 window.addEventListener("load", iniciar, false);
