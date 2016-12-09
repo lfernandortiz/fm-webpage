4fe2871e3503fbae7db905ecad62dfae55da8021
lfernandortiz <lfernandortiz@gmail.com>
2016-12-09 14:21:05 -0500

    Adicion de google analytics

--- js/vendor/foundation.js
+++ js/vendor/foundation.js
@@ -165,6 +165,7 @@
 
       // Iterate through each plugin
       $.each(plugins, function (i, name) {
+        console.log(name);
         // Get the current plugin
         var plugin = _this._plugins[name];
 
@@ -673,7 +674,7 @@
         winRect = document.body.getBoundingClientRect(),
         winY = window.pageYOffset,
         winX = window.pageXOffset;
-
+        console.log("X " + winX + "Y: " +winY );
     return {
       width: rect.width,
       height: rect.height,
