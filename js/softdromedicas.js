1b1cfa7ba39183f4edd94bd96d86afc5934cfed2
lfernandortiz <lfernandortiz@gmail.com>
2017-07-21 10:32:57 -0500

    adicion de nuevas sucursales

--- js/softdromedicas.js
+++ js/softdromedicas.js
@@ -56,7 +56,9 @@ var sucursales = [
 	['Farmanorte 28', 7.88749215, -72.50609315, 'Av 7 Calle 9 Esquina Centro', '5727091','3155997098', 'CUCUTA','','7am', '20', '8am', '2pm', 29],
 	['Farmanorte 29', 7.90146225, -72.48545421, 'Calle 2N #12E-20 AV Libertadores', '5777135','3125639874','CUCUTA','true','','','','',30],
 	['Farmanorte 30', 7.82706424, -72.51198608, 'Autopista los Patios Calle 16 Barrio Once de Noviembre', '5807646','3125480643','PATIOS','true','','','','',31],
-	['Farmanorte 32', 7.83258351, -72.47115523, 'Calle 5 #6-79 Villa del Rosario', '','3108506632','VILLA DEL ROSARIO','true','','','','',32]
+	['Farmanorte 31', 7.89229945, -72.48921045, 'Avenida 11e # 7-49 A 100mts De La Clinica Santa Ana Barrio  Colsag', '5773233','3108584088','CUCUTA','true','','','','',32],
+	['Farmanorte 32', 7.83258351, -72.47115523, 'Calle 5 #6-79 Villa del Rosario', '','3108506632','VILLA DEL ROSARIO','','8am', '22:00', '8am', '2pm',33],
+	['Farmanorte 33', 7.88242446, -72.4987313, 'Avenida 1 #15-03  Junto A La Clinica Jerico Barrio La Playa', '5710314','3105721715','Cucuta','true','', '', '', '',34]
 
 ];
 //funcion llamada al final por el registro de evento load del objeto window
