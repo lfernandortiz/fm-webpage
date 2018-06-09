7da1f895a83fe2fe7c3486a09fc5b7a98f318caf
lfernandortiz <lfernandortiz@gmail.com>
2018-06-09 11:59:21 -0500

    BreakPoint para iPhone 5

--- js/appm.js
+++ js/appm.js
@@ -1,13 +1,15 @@
 /**| @lfernandortiz - @SoftDromedicas*/
 $(document).foundation();
+console.log("%cDesarrollado por @Softdromedicas - @lfernandortiz con ❤️", "background: #000a7b; color: white; font-size: normal");
 
-var currentElement = null;
 
 //burger menu movil
 $(".btn-nav").on("click tap", function(){
 	$(".bar").toggleClass("animated");
  })
 
+
+ //mantiene la ventana del chat en la aplicacion
  $('#consulta-div').on('click', function() {
     window.open('https://tawk.to/chat/585959ccddb8373fd2b14f8e/default/?$_tawk_popout=false', 
                             '_blank', 'location=no,hidden=yes,closebuttoncaption=Done,toolbar=no');
